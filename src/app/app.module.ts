5f0e08682dc086a8af5b7175a700f39729dd561d
Radumgeid <36445562+Radumgeid@users.noreply.github.com>
2019-03-30 10:19:47 +0100

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { FirebaseDbProvider } from '../providers/firebase-db/firebase-db';
+
+export const fireBaseConfig={
+	apiKey: "AIzaSyDR98UZlU2UdyEIxE_Xj_gMnei7AifCduU",
+    authDomain: "doctonlinebd.firebaseapp.com",
+    databaseURL: "https://doctonlinebd.firebaseio.com",
+    projectId: "doctonlinebd",
+    storageBucket: "doctonlinebd.appspot.com",
+    messagingSenderId: "770817091146"};
+
+	
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+	AngularFireModule.initializeApp(fireBaseConfig),AngularFireDatabaseModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseDbProvider
+  ]
+})
+export class AppModule {}

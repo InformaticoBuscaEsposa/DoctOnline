9122b419363b559341bcc73e1b87eb802820b489
Radumgeid <36445562+Radumgeid@users.noreply.github.com>
2019-04-05 16:42:29 +0200

    Add files via upload

---
+++ src/models/usuario.model.ts
@@ -0,0 +1,17 @@
+export class Usuario {
+
+    public nombre: string;
+    public apellidos: string;
+    public tipo: string;
+    public dni: string;
+    public correo: string;
+	public clave: string;
+	public nSeguridadSocial: string;
+	public titulo: string;
+	
+	constructor()
+	{
+		this.dni="";
+	}
+    
+}
